c72cbbfc076b64b8c238554d3c687e3aed023549
Khushi Yadav <ky0307005@gmail.com>
2025-12-10 22:14:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Search a node in BST/search-a-node-in-bst.cpp
@@ -0,0 +1,29 @@
+/*
+class Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int x) {
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    bool search(Node* root, int key) {
+        // code here
+        if(!root) return 0;
+        
+        if(root->data == key) return 1;
+        
+        if(root->data > key){
+            search(root->left, key);
+        } else {
+            search(root->right, key);
+        }
+    }
+};
