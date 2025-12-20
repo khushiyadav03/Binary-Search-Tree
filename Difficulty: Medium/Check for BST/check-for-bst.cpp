73ed9dc0cb782182b44269dae7ec69532a37a454
Khushi Yadav <ky0307005@gmail.com>
2025-12-21 00:50:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Check for BST/check-for-bst.cpp
+++ Difficulty: Medium/Check for BST/check-for-bst.cpp
@@ -14,22 +14,20 @@ public:
 
 class Solution {
   public:
-    void inorder(Node* root, vector<int>& ans){
-        if(!root) return;
-        inorder(root->left, ans);
-        ans.push_back(root->data);
-        inorder(root->right, ans);
+    bool BST(Node* root, int& prev){
+        if(!root) return 1;
+        
+        bool l = BST(root->left, prev);
+        if(l == 0) return 0;
+        if(root->data <= prev) return 0;
+        
+        prev = root->data;
+        return BST(root->right, prev);
     }
   
     bool isBST(Node* root) {
         // code here
-        vector<int> ans;
-        inorder(root, ans);
-        for(int i = 1; i<ans.size(); i++){
-            if(ans[i] <= ans[i-1]){
-                return 0;
-            }
-        }
-        return 1;
+        int prev = INT_MIN;
+        return BST(root, prev);
     }
 };
