51d13fbd56dedfa9f17f03cf0ebf47e8c5b8f731
Khushi Yadav <ky0307005@gmail.com>
2025-12-29 00:38:44 +0530

    Time: 0 ms (100%), Space: 65.1 MB (28.45%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.cpp
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.cpp
@@ -11,32 +11,19 @@
  */
 class Solution {
 public:
-    void find(TreeNode* root, int low, int high, vector<int>& ans){
-        if(!root) return;
-
-        if(root->val > low && root->val > high){
-            return find(root->left, low, high, ans);
-        } else if(root->val < low && root->val < high){
-            return find(root->right, low, high, ans);
-        } else {
-            find(root->left, low, high, ans);
-            ans.push_back(root->val);
-            find(root->right, low, high, ans);
-        }
-    }
-
-    int sum(vector<int>& ans){
-        int s = 0;
-        for(int n : ans){
-            s += n;
-        }
-        return s;
-    }
-
     int rangeSumBST(TreeNode* root, int low, int high) {
-        vector<int> ans;
-        int s = 0;
-        find(root, low, high, ans);
-        return sum(ans);
+        if(!root) return 0;
+
+        if(root->val> high){
+            return rangeSumBST(root->left, low, high);
+        } 
+        
+        if(root->val < low){
+            return rangeSumBST(root->right, low, high);
+        }
+
+        return root->val + 
+            rangeSumBST(root->left, low, high) +
+            rangeSumBST(root->right, low, high);
     }
 };
